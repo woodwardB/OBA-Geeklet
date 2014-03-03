6b159f6afdf54615a841dd14a89cb66b08362d5d
Brandon Woodward <brandon@brandonwoodward.info>
2014-03-02 17:38:39 -0800

    added testing for pulling information on stops

--- oba-geeklet.py
+++ oba-geeklet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import ConfigParser
-import 
+import urllib2 
 
 Config = ConfigParser.ConfigParser()
 Config.read("config.ini")
@@ -22,16 +22,34 @@ def ConfigSectionMap(section):
 
 # Sample REST call
 # http://api.pugetsound.onebusaway.org/api/where/arrival-and-departure-for-stop/1_75403.xml?key=TEST&tripId=1_15551341&serviceDate=1291536000000&vehicleId=1_3521&stopSequence=42
-#
+# http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/1_75403.xml?key=TEST
+def CurrentArrivalsAndDepatruresForStop(stop, key):
+  # route 304
+  # stop 75190 W-bound 304
+  # 6950 S-bound E-Line
+  url = "http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/1_" 
+  urlreq = url + str(stop) + ".xml?key=" + key
+  print urlreq
+  req = urllib2.Request(urlreq)
+  response = urllib2.urlopen(req)
+  return response.read()
 
-def CurrentArrivalsAndDepatrures(stop, route):
-
-
-
+#Given a Stop and Key will return .xml formatted info about that stop
+def StopInfo(stop, key):
+  #http://api.pugetsound.onebusaway.org/api/where/stop/1_75403.xml?key=TEST
+  url = "http://api.pugetsound.onebusaway.org/api/where/stop/1_"
+  urlreq = url + str(stop) + ".xml?key=" + key
+  print urlreq
+  req = urllib2.Request(urlreq)
+  response = urllib2.urlopen(req)
+  return response.read() 
 
 def main():
   key = ConfigSectionMap("OBA")['key']
   print "OBA access key : %s" % (key)
+  stop = 6950
+  #print StopInfo(stop, key)
+  print CurrentArrivalsAndDepatruresForStop(stop, key)
 
 if __name__ == "__main__":
   main()
